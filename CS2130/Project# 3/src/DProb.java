20e3c547b591b59971512b4e09818157d2d1ff19
beretverde <tylerjacox@gmail.com>
2017-07-06 14:19:04 -0600

    Permutations

--- CS2130/Project# 3/src/DProb.java
+++ CS2130/Project# 3/src/DProb.java
@@ -2,14 +2,23 @@
 public class DProb
 {
 
+    public static double Factorial(long N){
+
+        double fact=1.0;
+        for (int i=1;i<=N;i++){
+            fact *=i;
+        }
+        return fact;
+    }
+
+
     public static double Permutations(long N, long X)
     {
         double perm = 1.0;
 
-        for (int x=0;x<=X;x++){
-            perm = perm*(double)(N-i);
+        for (int x=0;x<X;x++){
+            perm = perm*(double)(N-x);
         }
-
         return perm;
     }
 
@@ -17,11 +26,11 @@ public class DProb
     {
         double comb = 0.0;
 
-        if(x==0|X==N){
+        if(X==0|X==N){
             return 1.0;
         }
 
-
+        comb=Permutations(N,X)/ Factorial(X);
 
         return comb;
     }
---
+++ CS2130/Project# 3/src/main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jacoxt on 7/6/2017.
+ */
+public class main {
+
+    public static void main( String[] args ) {
+        System.out.println( DProb.Permutations(15,3) );
+        System.out.println( DProb.Factorial(3) );
+        System.out.println( DProb.Combinations(15,3) );
+
+        System.exit( 0 ); //success
+    }
+
+
+}
