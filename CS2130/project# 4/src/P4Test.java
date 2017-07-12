73f59b4d25b7013319250886020769d7d02c799c
Tyler <tylerjacox@gmail.com>
2017-07-12 15:08:06 -0600

    P4Test

--- CS2130/project# 4/src/P4Test.java
+++ CS2130/project# 4/src/P4Test.java
@@ -63,9 +63,41 @@ public class P4Test
                         {0, 0, 0, 0, 0, 0, 0, 0, 0},
                         {0, 0, 0, 0, 0, 0, 0, 0, 0},
                         {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        BMat A1, A2, A3,A4,A5,B1,B2,B3,B4,B5;
+
+
 
         BMat BMA = new BMat(A);
+        BMat BMB = new BMat(B);
+        BMat BMC = new BMat(C);
+        BMat BMD = new BMat(D);
+        BMat BME = new BMat(E);
+        BMat BMF = new BMat(F);
+        BMat BMG = new BMat(G);
+
         // Put code here...
+
+        //a.	W = (C' and (A or B)) and B'    (M' = complement of M)
+
+
+
+
+        A1=BMC.complement();
+        A2=BMA.join(BMB);
+        A3=BMB.complement();
+        A4=A1.meet(A2);
+        A5=A4.meet(A3);
+        System.out.println("W = (C' and (A or B)) and B");
+        A5.show();
+
+        //b. (B^T boolProduct B) and (C or C^T)
+
+      
+
+
+
+
+
 
     } // end main
 
