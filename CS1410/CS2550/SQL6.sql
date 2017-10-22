2e1d353e24d5844edd86389401f577bc5537869f
Tyler <tylerjacox@gmail.com>
2017-10-21 18:17:50 -0600

    Assignment6

--- CS1410/CS2550/SQL6.sql
+++ CS1410/CS2550/SQL6.sql
@@ -3,47 +3,47 @@
 --SQL Assignment #6
 
 --1
-SELECT x.student_id
-FROM enrollment x, section y
-WHERE y.section_id = x.section_id
-      AND y.course_no BETWEEN 100 AND 199
-GROUP BY x.student_id
+SELECT e.student_id
+FROM enrollment e, section s
+WHERE s.section_id = e.section_id
+      AND s.course_no BETWEEN 100 AND 199
+GROUP BY e.student_id
 HAVING COUNT(*) =
        (SELECT MAX(num_enrolled)
         FROM
           (SELECT
-             x.student_id,
+             e.student_id,
              COUNT(*) AS num_enrolled
-           FROM section y, enrollment x
-           WHERE y.section_id = x.section_id
-                 AND y.course_no BETWEEN 100 AND 199
-           GROUP BY x.student_id
+           FROM section s, enrollment e
+           WHERE s.section_id = e.section_id
+                 AND s.course_no BETWEEN 100 AND 199
+           GROUP BY e.student_id
            ORDER BY COUNT(*)));
 
 --2
 SELECT
-  x.student_id,
-  z.last_name,
-  z.first_name
-FROM grade x, enrollment y, student z, section zz
-WHERE x.student_id = x.student_id
-      AND x.student_id = z.student_id
-      AND x.section_id = zz.section_id
-      AND x.grade_type_code = 'FI'
-      AND zz.course_no = 230
-      AND zz.section_id = 100
-      AND x.numeric_grade =
+  g.student_id,
+  s.last_name,
+  s.first_name
+FROM grade g, enrollment e, student s, section sec
+WHERE g.student_id = g.student_id
+      AND g.student_id = s.student_id
+      AND g.section_id = sec.section_id
+      AND g.grade_type_code = 'FI'
+      AND sec.course_no = 230
+      AND sec.section_id = 100
+      AND g.numeric_grade =
           (SELECT MAX(maxGrade)
            FROM
-             (SELECT x.numeric_grade AS maxGrade
-              FROM grade x, enrollment y, student z, section zz
-              WHERE x.student_id = x.student_id
-                    AND x.student_id = z.student_id
-                    AND x.section_id = zz.section_id
-                    AND x.grade_type_code = 'FI'
-                    AND zz.course_no = 230
-                    AND zz.section_id = 100))
-GROUP BY x.student_id, z.first_name, z.last_name;
+             (SELECT g.numeric_grade AS maxGrade
+              FROM grade g, enrollment e, student s, section sec
+              WHERE g.student_id = g.student_id
+                    AND g.student_id = s.student_id
+                    AND g.section_id = sec.section_id
+                    AND g.grade_type_code = 'FI'
+                    AND sec.course_no = 230
+                    AND sec.section_id = 100))
+GROUP BY g.student_id, s.first_name, s.last_name;
 
 --3
 SELECT
@@ -59,26 +59,26 @@ ORDER BY last_name;
 
 --4
 SELECT
-  z.last_name,
-  x.numeric_grade
-FROM grade x, enrollment y, student z, section zz
-WHERE x.student_id = x.student_id
-      AND x.student_id = z.student_id
-      AND x.section_id = zz.section_id
-      AND x.grade_type_code = 'FI'
-      AND zz.section_id = 154
-      AND x.numeric_grade >
+  s.last_name,
+  g.numeric_grade
+FROM grade g, enrollment e, student s, section sec
+WHERE g.student_id = g.student_id
+      AND g.student_id = s.student_id
+      AND g.section_id = sec.section_id
+      AND g.grade_type_code = 'FI'
+      AND sec.section_id = 154
+      AND g.numeric_grade >
           (SELECT AVG(avgGrade)
            FROM
-             (SELECT x.numeric_grade AS avgGrade
-              FROM grade x, enrollment y, student z, section zz
-              WHERE x.student_id = x.student_id
-                    AND x.student_id = z.student_id
-                    AND x.section_id = zz.section_id
-                    AND x.grade_type_code = 'FI'
-                    AND zz.section_id = 154))
-GROUP BY z.last_name, x.numeric_grade
-ORDER BY z.last_name;
+             (SELECT g.numeric_grade AS avgGrade
+              FROM grade g, enrollment e, student s, section sec
+              WHERE g.student_id = g.student_id
+                    AND g.student_id = s.student_id
+                    AND g.section_id = sec.section_id
+                    AND g.grade_type_code = 'FI'
+                    AND sec.section_id = 154))
+GROUP BY s.last_name, g.numeric_grade
+ORDER BY s.last_name;
 
 --5
 
@@ -86,23 +86,23 @@ SELECT
   C.COURSE_NO,
   C.DESCRIPTION
 FROM section s
-  JOIN ENROLLMENT E
+  JOIN ENROLLMENT e
     ON s.section_id = e.section_id
   JOIN course c
     ON c.course_no = s.course_no
-GROUP BY C.COURSE_NO, C.DESCRIPTION
+GROUP BY c.COURSE_NO, c.DESCRIPTION
 HAVING count(*) = (SELECT MAX(num)
                    FROM (SELECT
-                           C.COURSE_NO,
-                           C.DESCRIPTION,
+                           c.COURSE_NO,
+                           c.DESCRIPTION,
                            count(*) AS num
                          FROM section s
                            JOIN ENROLLMENT E
                              ON s.section_id = e.section_id
                            JOIN course c
                              ON c.course_no = s.course_no
-                         GROUP BY C.COURSE_NO, C.DESCRIPTION))
-ORDER BY C.COURSE_NO;
+                         GROUP BY c.COURSE_NO, c.DESCRIPTION))
+ORDER BY c.COURSE_NO;
 
 --6
 
@@ -136,32 +136,32 @@ WHERE s.section_id = 135
 
 --8
 SELECT DISTINCT
-  std.first_name,
-  std.last_name,
-  std.phone
-FROM student std
+  s.first_name,
+  s.last_name,
+  s.phone
+FROM student s
   JOIN enrollment e
-    ON e.student_id = std.student_id
-WHERE std.student_id IN (SELECT student_id
+    ON e.student_id = s.student_id
+WHERE s.student_id IN (SELECT student_id
                          FROM enrollment e
-                           JOIN section s
-                             ON e.section_id = s.section_id
-                         WHERE s.section_id IN (SELECT section_id
-                                                FROM section s
+                           JOIN section sec
+                             ON e.section_id = sec.section_id
+                         WHERE sec.section_id IN (SELECT section_id
+                                                  FROM section sec
                                                   JOIN course c
-                                                    ON s.course_no = c.course_no
-                                                WHERE s.course_no IN (SELECT course_no
-                                                                      FROM course
+                                                    ON sec.course_no = c.course_no
+                                                WHERE sec.course_no IN (SELECT course_no
+                                                                        FROM course
                                                                       WHERE description = 'Project Management')))
-      AND std.student_id IN (SELECT student_id
+      AND s.student_id IN (SELECT student_id
                              FROM enrollment e
-                               JOIN section s
-                                 ON e.section_id = s.section_id
-                             WHERE s.section_id IN (SELECT section_id
-                                                    FROM section s
+                               JOIN section sec
+                                 ON e.section_id = sec.section_id
+                             WHERE sec.section_id IN (SELECT section_id
+                                                    FROM section sec
                                                       JOIN course c
-                                                        ON s.course_no = c.course_no
-                                                    WHERE s.course_no IN (SELECT course_no
+                                                        ON sec.course_no = c.course_no
+                                                    WHERE sec.course_no IN (SELECT course_no
                                                                           FROM course
                                                                           WHERE description = 'Systems Analysis')));
 
@@ -184,49 +184,33 @@ WHERE c.description LIKE '%Java%'
                                   ON s.course_no = c.course_no
                               WHERE c.description LIKE '%Java%'
                               GROUP BY s.instructor_id
-                              HAVING count(*) = (SELECT MAX(num)
+                              HAVING count(*) = (SELECT MAX(counter)
                                                  FROM (SELECT
                                                          s.instructor_id,
-                                                         COUNT(*) AS num
+                                                         COUNT(*) AS counter
                                                        FROM section s
                                                          JOIN course c
                                                            ON s.course_no = c.course_no
                                                        WHERE c.description LIKE '%Java%'
                                                        GROUP BY s.instructor_id)))
 ORDER BY i.FIRST_NAME, c.DESCRIPTION;
--- 10
+
+--10 .....rabbit hole of complexity
 
 SELECT DISTINCT
-  s.student_id,
-  s.last_name
-FROM student s
-  JOIN Grade g
-    ON s.student_id = g.student_id
-  JOIN section se
-    ON se.section_id = g.section_id
-WHERE se.section_id = 143 AND g.grade_type_code = 'FI'
-      AND g.numeric_grade > (SELECT AVG(g.numeric_grade)
-                             FROM section s
-                               JOIN grade g
-                                 ON s.section_id = g.section_id
-                             WHERE s.section_id = 143
-                                   AND g.grade_type_code = 'FI');
+  e.student_id,
+  s.last_name,
+  g.numeric_grade
 
---10 missing averages
+FROM enrollment e, student s, grade g
+WHERE e.student_id = s.student_id
+      AND e.student_id = g.student_id
+      AND e.section_id = 143
+      AND g.GRADE_TYPE_CODE='FI'
+GROUP BY e.student_id, s.last_name, g.numeric_grade
+ORDER BY e.student_id;
 
-SELECT
-  x.student_id,
-  y.last_name,
-  z.numeric_grade
-FROM enrollment x, student y, grade z
-WHERE x.student_id = y.student_id
-      AND x.student_id = z.student_id
-      AND x.section_id = 143
-
-GROUP BY x.student_id, y.last_name, z.numeric_grade
-ORDER BY x.student_id;
-
--- Test
+-- Attempts at #10
 SELECT DISTINCT s.student_id, s.last_name
 FROM student s JOIN Grade g
 ON s.student_id = g.student_id
@@ -255,7 +239,7 @@ AND g.grade_type_code = 'FI'
 ORDER BY s.last_name;
 
 
-SELECT STUDENT_ID
+SELECT *
   FROM GRADE
 WHERE SECTION_ID=143;
 
@@ -263,6 +247,26 @@ SELECT *
   FROM GRADE_TYPE_WEIGHT
 WHERE SECTION_ID=143;
 
+SELECT s.LAST_NAME,e.STUDENT_ID,g.NUMERIC_GRADE
+  FROM STUDENT s, ENROLLMENT e, GRADE g
+    JOIN ENROLLMENT e
+    ON e.STUDENT_ID=s.STUDENT_ID
+WHERE e.SECTION_ID=143;
 
 
+SELECT DISTINCT
+  s.student_id,
+  s.last_name
+FROM student s
+  JOIN Grade g
+    ON s.student_id = g.student_id
+  JOIN section se
+    ON se.section_id = g.section_id
+WHERE se.section_id = 143 AND g.grade_type_code = 'FI'
+      AND g.numeric_grade > (SELECT AVG(g.numeric_grade)
+                             FROM section s
+                               JOIN grade g
+                                 ON s.section_id = g.section_id
+                             WHERE s.section_id = 143
+                                   AND g.grade_type_code = 'FI');
 
